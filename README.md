71ac49c7b05d774a1318f47b7b32ccf666e3feb1
Tamil.Selvan <tamilzhchelvan@gmail.com>
2021-12-14 23:06:02 +0800

    update

--- README.md
+++ README.md
@@ -46,4 +46,6 @@ function App() {
       </div> 
     );
 }
+
+export default App;
 ```
--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import ReddevSlider from './lib/components/ReddevSlider';
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@emotion/react';
-import { useState, useEffect, useRef } from "react";
 
 
-
-const theme = createTheme({
-  status: {
-    danger: '#e53e3e',
-  },
-  palette: {
-    red: {
-      main: '#F46534',
-      darker: '#053e85',
-    },
-  },
-});
 
 //configure sliderValues
 
@@ -40,57 +25,20 @@ const sliders = [{
 }]
 
 function App() {
-
-  //state to manage all sliders
-  const [value, setValue] = useState(0);
-  //state to track which sliderValues is moved
-  const [sno, setSno] = useState(0);
   //state to share sliderValues value across all sliderValues
   const [sliderValues, setSlider] = useState([40,30,20,10]);
-  //Manage only on updates to avoid intial load update
-  const isInitialMount = useRef(true);
 
-  const handleChange = (sno, e) => {
-    console.log(e.target.value);
-    setValue(e.target.value);
-    setSno(sno);
+  const handleChange = (index, e) => {
+    let updatedSlider = [...sliderValues];
+    updatedSlider[index] = e.target.value
+    setSlider(updatedSlider);
   }
 
-  useEffect(()=>{
-    if(isInitialMount.current){
-      isInitialMount.current = false;
-    }else {
-      console.log(`Existing all silder ${sliderValues}`);
-      let existingAllSliderArray = sliderValues;
-      let existingSelectedSliderValue = existingAllSliderArray[sno];
-      let difference = Math.abs(existingSelectedSliderValue - value);
-      //Below sliderValues
-      let newAllSliderArray = [];
-      newAllSliderArray[sno] = value;
-      let total = existingAllSliderArray.slice(Number(sno+1), (existingAllSliderArray.length)).reduce((a,b)=>a+b,0);
-      for(let i=sno+1;i<existingAllSliderArray.length;i++){
-        if(total) {
-          newAllSliderArray[i] = Number(Math.floor((existingAllSliderArray[i]/total) * difference));
-        }
-      }
-      setSlider(newAllSliderArray);
-
-      console.log(`Existing selected sliderValues ${existingSelectedSliderValue}`);
-      console.log(`Difference ${difference}`);
-      console.log(`Total ${total} `);
-      console.log(`Final of all sliderValues ${newAllSliderArray}`);
-    }
-  }, [value])
-  
-  
-  
   return ( 
       <div className = "App">
         <div className="cont">
           <div className="sliders">
-            <ThemeProvider theme={theme}>
               <ReddevSlider sliders={sliders} onChange={handleChange} sliderValues={sliderValues} />
-            </ThemeProvider>
           </div>
         </div>
       </div> 
